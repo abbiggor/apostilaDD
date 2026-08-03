879b4b48966a18824517b5b75dc25464f04a30e1
unknown <mffsho@gmail.com>
2026-08-03 17:53:57 -0300

    # implementada a action 'Edit' # action 'Index' exibe os itens ordenados por ordem alfabética

--- solucao_capitulo01/capitulo01/Controllers/InstituicaoController.cs
+++ solucao_capitulo01/capitulo01/Controllers/InstituicaoController.cs
@@ -47,7 +47,7 @@ namespace capitulo01.Controllers
         }
         public IActionResult Index()
         {
-            return View(instituicoes);
+            return View(instituicoes.OrderBy(i => i.Nome));
         }
 
         [HttpPost]
@@ -56,6 +56,19 @@ namespace capitulo01.Controllers
         {
             instituicoes.Add(instituicao);
             instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
             return RedirectToAction("Index");
         }
     }
