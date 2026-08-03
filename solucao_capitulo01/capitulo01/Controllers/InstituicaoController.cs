2039b1d5c662b4a9ca9d4ad9742d3d9e262b718f
unknown <mffsho@gmail.com>
2026-08-03 16:40:14 -0300

    first commit

---
+++ solucao_capitulo01/capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,62 @@
+﻿using capitulo01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes =
+            new List<Instituicao>()
+            {
+                new Instituicao()
+                {
+                    InstituicaoID = 1,
+                    Nome ="UniParana",
+                    Endereco = "Parana"
+                },
+                new Instituicao()
+                {
+                    InstituicaoID = 2,
+                    Nome = "UniSanta",
+                    Endereco = "Santa Catarina"
+                },
+                new Instituicao()
+                {
+                    InstituicaoID= 3,
+                    Nome = "UniRS",
+                    Endereco = "Rio Grande do Sul"
+                },
+                new Instituicao()
+                {
+                    InstituicaoID = 4,
+                    Nome = "UniSP",
+                    Endereco = "São Paulo"
+                },
+                new Instituicao()
+                {
+                    InstituicaoID = 5,
+                    Nome = "UniMG",
+                    Endereco = "Minas Gerais"
+                }
+
+            };
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
