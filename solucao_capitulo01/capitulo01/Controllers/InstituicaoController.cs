279e0adf9dba074cdf98d92395752db0d2b89e6e
unknown <mffsho@gmail.com>
2026-08-04 13:46:25 -0300

    feat: implementa view de detalhes dos itens da lista e método delete, finalizando o projeto do primeiro capítulo da apostila

--- solucao_capitulo01/capitulo01/Controllers/InstituicaoController.cs
+++ solucao_capitulo01/capitulo01/Controllers/InstituicaoController.cs
@@ -58,17 +58,38 @@ namespace capitulo01.Controllers
             instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
             return RedirectToAction("Index");
         }
-        public ActionResult Edit(long id)
+        public ActionResult Edit(long id) // Esta action Edit faz um método GET, que recebe o id da instituição e retorna a view com os dados da instituição para edição
         {
             return View(instituicoes.Where(i => i.InstituicaoID == id).First());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Instituicao instituicao)
+        public ActionResult Edit(Instituicao instituicao) // Método POST
+        {
+            //instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            //instituicoes.Add(instituicao);
+            // o código acima remove o registro selecionado e adiciona um novo com os dados atualizados
+            // enquanto o código ativo abaixo atualiza o registro selecionado
+
+            instituicoes[instituicoes.IndexOf(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First())] = instituicao;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id) {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
         {
             instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
-            instituicoes.Add(instituicao);
             return RedirectToAction("Index");
         }
     }
