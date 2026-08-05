fb9f230f20eac2a909b2e12cb94fb2d9c9482b11
unknown <mffsho@gmail.com>
2026-08-05 17:52:57 -0300

    Conclusão do desafio proposto ao final do capítulo 2 de implementar o banco de dados na aplicação 1, porém decidi recriá-la ao invés de editá-la para massificar melhor os conceitos trabalhados anteriormete. O novo projeto está em 'Capitulo001'

---
+++ solucao_capitulo01/Capitulo001/Data/IESDbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Capitulo001.Data;
+using Capitulo001.Models;
+using System.Linq;
+namespace Capitulo001.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Instituicoes.Any())
+            {
+                return;
+            }
+            var instituicoes = new Instituicao[]
+            {
+                new Instituicao {Nome="UniPR", Endereco="Parana"},
+                new Instituicao {Nome="UniRS", Endereco="Rio Grande do Sul"}
+            };
+            foreach (Instituicao i in instituicoes)
+            {
+                context.Instituicoes.Add(i);
+            }
+            context.SaveChanges();
+        }
+    }
+}
