621090c7611a23f2a2141fa722aa8774e4e53a18
unknown <mffsho@gmail.com>
2026-08-12 14:46:09 -0300

    Criadas as classes 'Curso', 'Disciplina' e 'CursoDisciplina' na biblioteca de classes

---
+++ solucao/Modelo/Cadastros/Departamento.cs
@@ -0,0 +1,12 @@
+﻿namespace Modelo.Cadastros
+{
+    public class Departamento
+    {
+        public long? DepartamentoID { get; set; }
+        public string Nome { get; set; }
+
+        // Associação com a classe Instituicao
+        public long? InstituicaoID { get; set; }
+        public Instituicao? Instituicao { get; set; }
+    }
+}
