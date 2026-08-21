b25027244b6ddbda5f8723b8a27df86e79a6c7c9
unknown <mffsho@gmail.com>
2026-08-21 15:15:07 -0300

    Corrige erro da busca na base de dados das dropdown lits

---
+++ solucao/projeto_mvc/Migrations/20260821173942_ProfessoresAjuste.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projeto_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfessoresAjuste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CursoProfessor_Professor_ProfessorID",
+                table: "CursoProfessor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Professor",
+                table: "Professor");
+
+            migrationBuilder.RenameTable(
+                name: "Professor",
+                newName: "ProfessoresAjuste");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProfessoresAjuste",
+                table: "ProfessoresAjuste",
+                column: "ProfessorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CursoProfessor_ProfessoresAjuste_ProfessorID",
+                table: "CursoProfessor",
+                column: "ProfessorID",
+                principalTable: "ProfessoresAjuste",
+                principalColumn: "ProfessorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CursoProfessor_ProfessoresAjuste_ProfessorID",
+                table: "CursoProfessor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProfessoresAjuste",
+                table: "ProfessoresAjuste");
+
+            migrationBuilder.RenameTable(
+                name: "ProfessoresAjuste",
+                newName: "Professor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Professor",
+                table: "Professor",
+                column: "ProfessorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CursoProfessor_Professor_ProfessorID",
+                table: "CursoProfessor",
+                column: "ProfessorID",
+                principalTable: "Professor",
+                principalColumn: "ProfessorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ solucao/projeto_mvc/Migrations/20260821175508_ProfessoresAjuste2.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projeto_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfessoresAjuste2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CursoProfessor_ProfessoresAjuste_ProfessorID",
+                table: "CursoProfessor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProfessoresAjuste",
+                table: "ProfessoresAjuste");
+
+            migrationBuilder.RenameTable(
+                name: "ProfessoresAjuste",
+                newName: "Professores");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Professores",
+                table: "Professores",
+                column: "ProfessorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CursoProfessor_Professores_ProfessorID",
+                table: "CursoProfessor",
+                column: "ProfessorID",
+                principalTable: "Professores",
+                principalColumn: "ProfessorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CursoProfessor_Professores_ProfessorID",
+                table: "CursoProfessor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Professores",
+                table: "Professores");
+
+            migrationBuilder.RenameTable(
+                name: "Professores",
+                newName: "ProfessoresAjuste");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProfessoresAjuste",
+                table: "ProfessoresAjuste",
+                column: "ProfessorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CursoProfessor_ProfessoresAjuste_ProfessorID",
+                table: "CursoProfessor",
+                column: "ProfessorID",
+                principalTable: "ProfessoresAjuste",
+                principalColumn: "ProfessorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
